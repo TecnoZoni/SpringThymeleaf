d871e8c4c85f721408a32b3a78d0f69fba2e9424
Admin <agustinminzoni@gmail.com>
2024-08-10 18:31:29 -0300

    Completo

--- src/main/java/com/agustin/videojuegos/controller/ControladorVideojuego.java
+++ src/main/java/com/agustin/videojuegos/controller/ControladorVideojuego.java
@@ -4,15 +4,23 @@ import com.agustin.videojuegos.model.Videojuego;
 import com.agustin.videojuegos.services.CategoriaService;
 import com.agustin.videojuegos.services.EstudioService;
 import com.agustin.videojuegos.services.VideojuegoService;
+import jakarta.validation.Valid;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
 import java.util.List;
+import javax.imageio.ImageIO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ControladorVideojuego {
@@ -93,17 +101,58 @@ public class ControladorVideojuego {
     }
 
     @PostMapping("/formulario/videojuego/{id}")
-    public String guardarVideojuego(@ModelAttribute("videojuego") Videojuego videojuego, Model model, @PathVariable Long id) {
+    public String guardarVideojuego(
+            @RequestParam("archivo") MultipartFile archivo,
+            @Valid @ModelAttribute("videojuego") Videojuego videojuego,
+            BindingResult result,
+            Model model, @PathVariable("id") long id
+    ) {
+
         try {
+            model.addAttribute("categorias", this.svcCategoria.findAll());
+            model.addAttribute("estudios", this.svcEstudio.findAll());
+            if (result.hasErrors()) {
+                return "views/formulario/videojuego";
+            }
+            String ruta = "C://Videojuegos/imagenes";
+            int index = archivo.getOriginalFilename().indexOf(".");
+            String extension = "";
+            extension = "." + archivo.getOriginalFilename().substring(index + 1);
+            String nombreFoto = Calendar.getInstance().getTimeInMillis() + extension;
+            Path rutaAbsoluta = id != 0 ? Paths.get(ruta + "//" + videojuego.getImagen())
+                    : Paths.get(ruta + "//" + nombreFoto);
             if (id == 0) {
+                if (archivo.isEmpty()) {
+                    model.addAttribute("errorImagenMsg", "La imagen es requerida");
+                    return "views/formulario/videojuego";
+                }
+                if (!this.validarExtension(archivo)) {
+                    model.addAttribute("errorImagenMsg", "La extension no es valida");
+                    return "views/formulario/videojuego";
+                }
+                if (archivo.getSize() >= 15000000) {
+                    model.addAttribute("errorImagenMsg", "El peso excede 15MB");
+                    return "views/formulario/videojuego";
+                }
+                Files.write(rutaAbsoluta, archivo.getBytes());
+                videojuego.setImagen(nombreFoto);
                 this.svcVideojuego.saveOne(videojuego);
             } else {
+                if (!archivo.isEmpty()) {
+                    if (!this.validarExtension(archivo)) {
+                        model.addAttribute("errorImagenMsg", "La extension no es valida");
+                        return "views/formulario/videojuego";
+                    }
+                    if (archivo.getSize() >= 15000000) {
+                        model.addAttribute("errorImagenMsg", "El peso excede 15MB");
+                        return "views/formulario/videojuego";
+                    }
+                    Files.write(rutaAbsoluta, archivo.getBytes());
+                }
                 this.svcVideojuego.updateOne(videojuego, id);
             }
-
             return "redirect:/crud";
         } catch (Exception e) {
-            System.out.println(e.getMessage());
             model.addAttribute("error", e.getMessage());
             return "error";
         }
@@ -117,6 +166,27 @@ public class ControladorVideojuego {
         } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
             return "error";
+        }
+    }
+
+    @PostMapping("/eliminar/videojuego/{id}")
+    public String desactivarVideojuego(Model model, @PathVariable Long id) {
+        try {
+            this.svcVideojuego.deleteById(id);
+            return "redirect:/crud";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+    }
+
+    public boolean validarExtension(MultipartFile archivo) {
+        try {
+            ImageIO.read(archivo.getInputStream()).toString();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
         }
     }
 
